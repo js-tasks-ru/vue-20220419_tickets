5abaa0e74d246cb9e7e5e802284a66442c73d13f
Michail Zakharov <michail.zakharov@fastdev.se>
2022-05-01 13:30:40 +0400

    1-2

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+// Создайте Vue приложение
+const RootComponent = defineComponent({
+  name: 'RootComponent',
+
+  data() {
+    return {
+      operandOne: 0,
+      operandTwo: 0,
+      operator: 'multiply',
+    };
+  },
+  computed: {
+    calculation() {
+      let result;
+      switch (this.operator) {
+        case 'sum':
+          result = this.operandOne + this.operandTwo;
+          break;
+        case 'subtract':
+          result = this.operandOne - this.operandTwo;
+          break;
+        case 'multiply':
+          result = this.operandOne * this.operandTwo;
+          break;
+        case 'divide':
+          result = this.operandOne / this.operandTwo;
+          break;
+      }
+
+      return result;
+    },
+  },
+});
+
+const app = createApp(RootComponent);
+const vm = app.mount('#app');
