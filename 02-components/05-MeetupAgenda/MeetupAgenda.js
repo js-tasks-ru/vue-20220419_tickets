6878165baff753c46db4ad307c3a5be8eed3ad9d
Michail Zakharov <michail.zakharov@fastdev.se>
2022-05-04 12:28:28 +0400

    3-5

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,23 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
+  
+  components: {
+    MeetupAgendaItem,
+  },
+  props: {
+    agenda: {
+      type: Array,
+      requird: true,
+    },
+  },
 
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
+      <li v-for="item in agenda" class="agenda__item">
+        <meetup-agenda-item :agenda-item="item"></meetup-agenda-item>
       </li>
     </ul>`,
 });
--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,50 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  agendaItemIcons,
+  agendaItemDefaultTitles,
+
+  computed: {
+    getIcon() {
+      return `/assets/icons/icon-${this.$options.agendaItemIcons[this.agendaItem.type]}.svg`;
+    },
+
+    getTitle() {
+      return this.agendaItem.title
+        ? this.agendaItem.title
+        : this.$options.agendaItemDefaultTitles[this.agendaItem.type];
+    },
+    getTimes() {
+      return `${this.agendaItem.startsAt} - ${this.agendaItem.endsAt}`;
+    },
+  },
 
   template: `
+  <!-- {{agendaItem}} -->
+  <!-- {{$options.agendaItemIcons[agendaItem.type]}} -->
+  <!-- {{agendaItemIcons[agendaItem.type]}} -->
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="getIcon" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{getTimes}}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
+        <h3 class="agenda-item__title">{{getTitle}}</h3>
+        <p v-if = "agendaItem.type === 'talk'" class="agenda-item__talk">
           <span>Talk Speaker</span>
           <span class="agenda-item__dot"></span>
           <span class="agenda-item__lang">EN</span>
         </p>
-        <p>Description</p>
+        <p v-if="agendaItem.description">{{agendaItem.description}}</p>
       </div>
     </div>`,
 });
