fecac95d1e2c67b2df08c7716fabc4d3e1ec5111
Michail Zakharov <michail.zakharov@fastdev.se>
2022-05-11 18:35:18 +0400

    04-3 fix

--- 04-vue-cli/03-UiDropdown1/components/UiDropdown.vue
+++ 04-vue-cli/03-UiDropdown1/components/UiDropdown.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="dropdown dropdown_opened">
-    <button type="button" class="dropdown__toggle dropdown__toggle_icon">
+  <div class="dropdown" :class="{ dropdown_opened: opened }">
+    <button type="button" class="dropdown__toggle dropdown__toggle_icon" @click="opened = !opened">
       <ui-icon icon="tv" class="dropdown__icon" />
-      <span>Title</span>
+      <span>{{ dropdownTitle }}</span>
     </button>
 
-    <div class="dropdown__menu" role="listbox">
-      <button class="dropdown__item dropdown__item_icon" role="option" type="button">
-        <ui-icon icon="tv" class="dropdown__icon" />
-        Option 1
+    <div v-show="opened" class="dropdown__menu" role="listbox">
+      <button
+        v-for="option in options"
+        :key="option.value"
+        class="dropdown__item dropdown__item_icon"
+        role="option"
+        type="button"
+        @click="setActiveOption(option)"
+      >
+        <ui-icon :icon="option.icon" class="dropdown__icon" />
+        {{ option.text }}
       </button>
-      <button class="dropdown__item dropdown__item_icon" role="option" type="button">
+      <!-- <button class="dropdown__item dropdown__item_icon" role="option" type="button">
         <ui-icon icon="tv" class="dropdown__icon" />
         Option 2
-      </button>
+      </button> -->
     </div>
   </div>
 </template>
@@ -25,6 +32,40 @@ export default {
   name: 'UiDropdown',
 
   components: { UiIcon },
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      opened: false,
+      // selectedOption: null,
+    };
+  },
+  computed: {
+    dropdownTitle() {
+      return this.modelValue ? this.options.find((e) => e.value === this.modelValue).text : this.title;
+    },
+  },
+  methods: {
+    setActiveOption(option) {
+      console.log(option);
+      // this.selectedOption = option;
+      this.$emit('update:modelValue', option.value);
+      this.opened = false;
+    },
+  },
 };
 </script>
 
